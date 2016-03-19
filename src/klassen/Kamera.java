73748d87c081972522c1613a6313e22f91837a3d
larrywayn <larrywayn@outlook.com>
2016-03-19 23:11:15 +0100

    Kleinere Ausbesserungen

--- src/klassen/Kamera.java
+++ src/klassen/Kamera.java
@@ -1,5 +1,7 @@
 package klassen;
 
+import main.LarryEngineKern;
+
 /**
  * Created by larry on 13.03.2016.
  */
@@ -9,23 +11,21 @@ public class Kamera {
     private float[] perspektive;
     private float aspekt;
     public Kamera(float aspekt){
-        this(aspekt, new Vektor4(0, 0, 950, 1),  new Quaternion(0, 0, 0, 1));
+        this(aspekt, new Vektor4(0.0f, 0.0f, 0.0f, 0.0f),  new Quaternion(0.0f, 0.0f, 0.0f, 0.0f));
     }
     public Kamera(float aspekt, Vektor4 standort, Quaternion ausrichtung){
-        this.ausrichtung = new Quaternion(0, 0, 0, 1);
-        this.standort = new Vektor4(0, 0, 950, 1);
+        this.ausrichtung = ausrichtung;
+        this.standort = standort;
         this.aspekt = aspekt;
-        this.perspektive = erzeugePerspektive(Configuration.holSichtfeld(), this.aspekt, Configuration.holNear(),Configuration.holFar());
+        this.perspektive = erzeugePerspektive(Configuration.holSichtfeld(), Configuration.holNear(),Configuration.holFar());
     }
-    protected float[] erzeugePerspektive(float fieldOfViewInRadians, float aspect, float near, float far) {
-        float f = (float)Math.tan(Math.PI * 0.5f - 0.5f * fieldOfViewInRadians);
-        float rangeInv = 1.0f / (near - far);
+    protected float[] erzeugePerspektive(float fieldOfViewInRadians, float near, float far) {
+        float f = 1.0f/(float)Math.tan(fieldOfViewInRadians/2.0f);
         return new float[]{
-                f / aspect, 0.0f, 0.0f, 0.0f,
+                f / this.aspekt, 0.0f, 0.0f, 0.0f,
                 0.0f, f, 0.0f, 0.0f,
-                0.0f, 0.0f, (near + far) * rangeInv, -1.0f,
-                0.0f, 0.0f, near * far * rangeInv * 2.0f, 0.0f
-        };
+                0.0f, 0.0f, (near + far) /(near-far), (2.0f*far*near)/(near-far),
+                0.0f, 0.0f, -1.0f, 0.0f};
     };
 
     public Vektor4 holStandort(){
@@ -36,5 +36,23 @@ public class Kamera {
     }
     public float[] holPerspektive(){
         return this.perspektive;
+    }
+
+    public float[] holMatrix() {
+        Quaternion invertView = null;
+        Vektor4 invertStandort = null;
+        try {
+            invertView = LarryEngineKern.kopiereObject(this.holAusrichtung());
+            invertView.konjugante();
+            invertStandort = LarryEngineKern.kopiereObject(this.holStandort());
+          //  invertStandort.invertiere();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    //    invertView.invertiere();
+       // Vektor4 invertStandort = this.mapper.convertValue(this.holStandort(), Vektor4.class);
+
+       // return invertView.erzeugeMatrix(this.standort);
+        return this.holAusrichtung().erzeugeMatrix(this.standort);
     }
 }
