b5f3a5e3e9ef87d703886fb70d598ff74aa1995b
larrywayn <larrywayn@outlook.com>
2016-03-19 23:03:50 +0100

    Um Normalen ergänzt. eventuell noch falsche ausrichtung.

--- src/klassen/geometrie/Wuerfel.java
+++ src/klassen/geometrie/Wuerfel.java
@@ -24,20 +24,70 @@ public class Wuerfel extends Geometrie {
 				 1.0f, -1.0f, -1.0f, 
 				 1.0f, 1.0f, -1.0f, 
 				 -1.0f, 1.0f, -1.0f };
+		this.normalenPlain = new float[]{ // vorne
+				0.0f,  0.0f,  1.0f,
+				0.0f,  0.0f,  1.0f,
+				0.0f,  0.0f,  1.0f,
+
+				0.0f,  0.0f,  1.0f,
+				0.0f,  0.0f,  1.0f,
+				0.0f,  0.0f,  1.0f,
+
+				0.0f,  1.0f,  0.0f,
+				0.0f,  1.0f,  0.0f,
+				0.0f,  1.0f,  0.0f,
+
+				0.0f,  1.0f,  0.0f,
+				0.0f,  1.0f,  0.0f,
+				0.0f,  1.0f,  0.0f,
+
+				0.0f,  0.0f, -1.0f,
+				0.0f,  0.0f, -1.0f,
+				0.0f,  0.0f, -1.0f,
+
+				0.0f,  0.0f, -1.0f,
+				0.0f,  0.0f, -1.0f,
+				0.0f,  0.0f, -1.0f,
+
+				-1.0f,  0.0f,  0.0f,
+				-1.0f,  0.0f,  0.0f,
+				-1.0f,  0.0f,  0.0f,
+
+				-1.0f,  0.0f,  0.0f,
+				-1.0f,  0.0f,  0.0f,
+				-1.0f,  0.0f,  0.0f,
+
+				0.0f, -1.0f,  0.0f,
+				0.0f, -1.0f,  0.0f,
+				0.0f, -1.0f,  0.0f,
+
+				0.0f, -1.0f,  0.0f,
+				0.0f, -1.0f,  0.0f,
+				0.0f, -1.0f,  0.0f,
+
+				1.0f,  0.0f,  0.0f,
+				1.0f,  0.0f,  0.0f,
+				1.0f,  0.0f,  0.0f,
+
+				1.0f,  0.0f,  0.0f,
+				1.0f,  0.0f,  0.0f,
+				1.0f,  0.0f,  0.0f
+		};
 		this.indiciesPlain = new int[] { 0, 1, 2, 2, 3, 0, 
 				3, 2, 6, 6, 7, 3, 
 				7, 6, 5, 5, 4, 7, 
 				4, 0, 3, 3, 7, 4, 
 				0, 1, 5, 5, 4, 0,
 				1, 5, 6, 6, 2, 1 };
-		int[] vboHandles = new int[3];
-		gl.glGenBuffers(3, vboHandles, 0);
+		int[] vboHandles = new int[4];
+		gl.glGenBuffers(4, vboHandles, 0);
 		this.vertices = vboHandles[0];
 		this.uvs = vboHandles[1];
-		this.indicies = vboHandles[2];
-
+		this.normalen = vboHandles[2];
+		this.indicies = vboHandles[3];
 		LarryEngineKern.createBuffer(gl, this.vertices, this.erzeugeBuffer(this.verticesPlain));
 		LarryEngineKern.createBuffer(gl, this.uvs, this.erzeugeBuffer(this.uvsPlain));
+		LarryEngineKern.createBuffer(gl, this.normalen, this.erzeugeBuffer(this.normalenPlain));
 		LarryEngineKern.createBuffer(gl, this.indicies, this.erzeugeBuffer(this.indiciesPlain));
 	}
 }
